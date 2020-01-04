fdc9740f284eb11cbfc65aa7fda09ef5190c4de8
Maria <mariabrock414@gmail.com>
2020-01-04 13:25:13 -0600

    can logout

--- src/Components/shared/MyNavbar/MyNavbar.js
+++ src/Components/shared/MyNavbar/MyNavbar.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 class MyNavbar extends React.Component {
-  render() {
-    return (
-            <div className="MyNavbar">
-                <h1>Imma Navbar!</h1>
+    logMeOut = (e) => {
+      e.preventDefault();
+      firebase.auth().signOut();
+    }
+
+    render() {
+      const { authed } = this.props;
+
+      return (
+    <div className="MyNavbar">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Pinterest</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
+            <div class="my-2 mt-lg-0">
+              {authed && (<button className="nav-link btn btn-danger" onClick={this.logMeOut}>Logout</button>)}
             </div>
-    );
-  }
+        </div>
+    </nav>
+</div>
+      );
+    }
 }
 
 export default MyNavbar;
