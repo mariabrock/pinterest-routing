9a78529679e1205f6a58c3444fc2118e1e57b8ef
Maria <mariabrock414@gmail.com>
2020-01-04 11:01:41 -0600

    set up basic routing

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <button className="btn btn-danger">Bootstrap Button</button>
-    </div>
-  );
+import Auth from '../Components/pages/Auth/Auth';
+import Home from '../Components/pages/Home/Home';
+import NewBoard from '../Components/pages/NewBoard/NewBoard';
+import SingleBoard from '../Components/pages/SingleBoard/SingleBoard';
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+class App extends React.Component {
+  state = {
+    authed: true,
+  }
+
+  render() {
+    const { authed } = this.state;
+
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <PrivateRoute path="/" exact component={Home} authed={authed} />
+            <PrivateRoute path="/board/new" exact component={NewBoard} authed={authed} />
+            <PublicRoute path="/auth" exact component={Auth} authed={authed} />
+            <PrivateRoute path="/board/:boardId" exact component={SingleBoard} authed={authed} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
