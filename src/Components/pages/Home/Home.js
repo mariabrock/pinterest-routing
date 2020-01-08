32b55066c9ee6ced87bd352180d7d6b8224b1721
Maria <mariabrock414@gmail.com>
2020-01-07 19:52:58 -0600

    read on boards works

--- src/Components/pages/Home/Home.js
+++ src/Components/pages/Home/Home.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.scss';
 
+import boardData from '../../../helpers/data/boardData';
+import authData from '../../../helpers/data/authData';
+import Board from '../../shared/Boards/Boards';
+
 class Home extends React.Component {
+  state = {
+    boards: [],
+  }
+
+  componentDidMount() {
+    boardData.getBoardsByUid(authData.getUid())
+      .then((boards) => this.setState({ boards }))
+      .catch((err) => console.error('error from get boards', err));
+  }
+
   render() {
     const boardId = '12345';
 
     return (
             <div className="Home">
                 <h1>Home Page</h1>
-                <Link className="btn btn-success" to="/board/new">Create New Board</Link>
-                <Link className="btn btn-primary" to={`/board/${boardId}`}>Single Board Page</Link>
+                <div className="boards d-flex flex-wrap">
+                  {this.state.boards.map((board) => (<Board key={board.id} board={board} />))}
+                </div>
             </div>
     );
   }
