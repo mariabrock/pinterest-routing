e69c77142eab4f9683af81a34ec70258a3b78dab
Maria <mariabrock414@gmail.com>
2020-01-07 20:29:26 -0600

    read works on pins

--- src/Components/pages/SingleBoard/SingleBoard.js
+++ src/Components/pages/SingleBoard/SingleBoard.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import './SingleBoard.scss';
 
-class SingleBoard extends React.Component {
-  render() {
-    const { boardId } = this.props.match.params;
+import boardData from '../../../helpers/data/boardData';
+import pinData from '../../../helpers/data/pinData';
+import Pin from '../../shared/Pin/Pin';
 
+class SingleBoard extends React.Component {
+  state = {
+    board: {},
+    pins: [],
+  }
+
+  getPinData = (boardId) => {
+    pinData.getPinsByBoardId(boardId)
+      .then((pins) => this.setState({ pins }))
+      .catch((err) => console.error('error in get pins', err));
+  }
+
+  componentDidMount() {
+    const { boardId } = this.props.match.params;
+    boardData.getSingleBoard(boardId)
+      .then((response) => {
+        this.setState({ board: response.data });
+        this.getPinData(boardId);
+      })
+      .catch((err) => console.error('error in get singleboard', err));
+  }
+
+  render() {
+    const { board } = this.state;
     return (
             <div className="SingleBoard">
-                <h1>SingleBoard Page</h1>
-                <h2>Current BoardId is: {boardId}</h2>
+                <h1>{board.name}</h1>
+                <p>{board.description}</p>
+                <div className="pins d-flex flex-wrap">
+                  {this.state.pins.map((pin) => <Pin key={pin.id} pin={pin} />)}
+                </div>
             </div>
     );
   }
